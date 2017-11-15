50e517515d3598fe4538d8de6f19b6ef05a64d3c
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-11-15 06:41:40 -0700

    added navbar

---
+++ react-ui/src/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Image } from 'semantic-ui-react';
+import { inject, observer } from 'mobx-react';
+
+var Navbar = observer(class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      activeItem: 'home' 
+    } 
+  } 
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state;
+
+    let entryLinks = [];
+    
+    if (this.props.UserStore.user.firstName) {
+      entryLinks.push(<Link className="item" key='linkLogout' to="/logout">Logout</Link>);
+    } else {
+      entryLinks.push(<Link className="item" key='linkLogin' to="/login">Login</Link>);
+      entryLinks.push(<Link className="item" key='linkSignup' to="/signup">Signup</Link>);
+    }
+
+    return (
+      <div>
+        <Menu icon stackable> 
+          <Link className="item" to="/"> Running App
+          </Link>
+          <Link className="item" to="/about">About</Link>
+         
+          <Menu.Menu position='right'>
+            
+            {/* show user dashboard & logout */}
+            {entryLinks}
+            {/* if not logged in show these: */}
+                     
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+});
+
+export default inject("UserStore")(Navbar);
